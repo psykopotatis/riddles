831180b0c16ea13ad9ea22c3a1b75cbfd82d6f1d
ingemar.finn <ingemar.finn@king.com>
2023-04-08 10:34:57 +0200

    Hide answer until button click

--- src/quiz.js
+++ src/quiz.js
@@ -1,11 +1,30 @@
-fetch('/src/json/riddles_01.json')
-    .then(response => response.json())
-    .then(data => {
-        const quiz = document.getElementById('quiz');
-        data.forEach(question => {
-            const questionDiv = document.createElement('div');
-            questionDiv.innerHTML = `<h2>${question.question}</h2><p>${question.answer}</p>`;
-            quiz.appendChild(questionDiv);
-        });
-    })
-    .catch(error => console.error(error));
+const quiz = document.getElementById('quiz');
+const questionDiv = document.getElementById('question');
+const showAnswerButton = document.getElementById('show-answer');
+
+let currentQuestionIndex = 0;
+let questions;
+
+fetch('/src/json/riddles_02.json')
+  .then(response => response.json())
+  .then(data => {
+    questions = data;
+    showQuestion();
+  })
+  .catch(error => console.error(error));
+
+function showQuestion() {
+  const question = questions[currentQuestionIndex];
+  questionDiv.innerHTML = `<h2>${question.question}</h2>`;
+  showAnswerButton.style.display = 'block';
+}
+
+showAnswerButton.addEventListener('click', () => {
+  const answer = questions[currentQuestionIndex].answer;
+  const answerDiv = document.createElement('div');
+  answerDiv.innerHTML = `<p>${answer}</p>`;
+  answerDiv.style.opacity = 0;
+  questionDiv.appendChild(answerDiv);
+  setTimeout(() => answerDiv.style.opacity = 1, 100);
+});
+
